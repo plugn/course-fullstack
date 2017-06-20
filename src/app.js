8e6cf5b3a98917f623e21a1b9783f4ffd7917d7e
Maxim Dolgov <bananafishbone@gmail.com>
2017-06-20 18:17:26 +0300

    state CRUD

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import { createStore } from 'redux';
 
 // STEP 3 define reducers
-const reducer = function (state=0, action) {
+const reducer = function (state = {books:[]}, action) {
+console.log('action.payload', action.payload);
 
 	switch (action.type) {
-		case 'INCREMENT':
-			return state + action.payload;
-			break;
-		case 'DECREMENT':
-			return state - action.payload;
+		case 'POST_BOOK':
+			// return state.books.concat(action.payload);
+			let newState = {
+				books: [...state.books, ...action.payload]
+			};
+			console.log('new', newState);
+			return newState;
 			break;
 	}
 
@@ -23,6 +26,30 @@ store.subscribe(function () {
 });
 
 // STEP 2 create and dispatch actions
-store.dispatch({type: 'INCREMENT', payload: 1});
-store.dispatch({type: 'INCREMENT', payload: 1});
-store.dispatch({type: 'DECREMENT', payload: 1});
+store.dispatch({
+	type: 'POST_BOOK',
+	payload: [
+		{
+			id: 1,
+			title: 'book title 1',
+			description: 'book description 1',
+			price: 33.33
+		},
+		{
+			id: 2,
+			title: 'book title 2',
+			description: 'this is the book description 2',
+			price: 50.33
+		}
+	]
+});
+
+store.dispatch({
+	type: 'POST_BOOK',
+	payload: [{
+		id: 15,
+		title: 'Third',
+		description: '3rd',
+		price: 100
+	}]
+})
