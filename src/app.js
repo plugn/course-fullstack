902ebc9ab00b2529e7dbddd147ea0ed580ac4190
Maxim Dolgov <bananafishbone@gmail.com>
2017-06-20 17:50:29 +0300

    simple redux app

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux';
+
+// STEP 3 define reducers
+const reducer = function (state=0, action) {
+
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + action.payload;
+			break;
+		case 'DECREMENT':
+			return state - action.payload;
+			break;
+	}
+
+	return state;
+};
+
+// STEP 1 create the store
+const store = createStore(reducer);
+
+store.subscribe(function () {
+	console.log('current state is: ', store.getState());
+});
+
+// STEP 2 create and dispatch actions
+store.dispatch({type: 'INCREMENT', payload: 1});
+store.dispatch({type: 'INCREMENT', payload: 1});
+store.dispatch({type: 'DECREMENT', payload: 1});
