c2b5717983cc491d20d2480499d951a3dc9a1e81
Maxim Dolgov <bananafishbone@gmail.com>
2017-06-21 15:32:30 +0300

    reducers combined and moved from app.js; react-router@3.x

--- src/app.js
+++ src/app.js
@@ -1,67 +1,8 @@
-import { createStore } from 'redux';
-
-// STEP 3 define reducers
-const reducer = function (state = {books:[]}, action) {
-console.log('action.payload', action.payload);
-
-	switch (action.type) {
-		case 'POST_BOOK':
-			// return state.books.concat(action.payload);
-			let newState = {
-				books: [...state.books, ...action.payload]
-			};
-			console.log('new', newState);
-			return newState;
-		break;
-
-		case 'DELETE_BOOK':
-			// copy of books array
-			const currentBookToDelete = [...state.books];
-			const indexToDelete = currentBookToDelete.findIndex(function (book) {
-				return book.id === action.payload.id;
-			});
-			if (-1 === indexToDelete) { return state; }
-
-			// use slice to remove the book at the specified index
-			return {
-				books: [
-					...currentBookToDelete.slice(0, indexToDelete),
-					...currentBookToDelete.slice(indexToDelete + 1)
-				]
-			};
-		break;
-
-		case 'UPDATE_BOOK':
-			// copy of books array
-			const currentBookToUpdate = [...state.books];
-
-			const indexToUpdate = currentBookToUpdate.findIndex(function (book) {
-				return book.id === action.payload.id;
-			});
-
-			if (-1 === indexToUpdate) { return state; }
-
-			const newBookToUpdate = {
-				...currentBookToUpdate[indexToUpdate],
-				title: action.payload.title
-			};
-
-			// use slice to remove the book at the specified index
-			return {
-				books: [
-					...currentBookToUpdate.slice(0, indexToUpdate),
-					newBookToUpdate,
-					...currentBookToUpdate.slice(indexToUpdate + 1)
-				]
-			};
-		break;
-	}
-
-	return state;
-};
+import { createStore } from 'redux'
+import reducers from './reducers/index'
 
 // STEP 1 create the store
-const store = createStore(reducer);
+const store = createStore(reducers);
 
 store.subscribe(function () {
 	console.log('current state is: ', store.getState().books);
